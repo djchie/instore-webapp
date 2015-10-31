af0222086d7599c9930a8c411a3098f48b061954
Derrick J Chie <djchie.dev@gmail.com>
2015-10-31 02:11:40 -0700

    Starts implementation of the product search results view

--- client/js/components/product-search-result.js
+++ client/js/components/product-search-result.js
@@ -2,7 +2,14 @@ var React = require("react");
 var Router = require('react-router');
 var Navigation = require('react-router').Navigation;
 var State = require('react-router').State;
+var Link = Router.Link;
 
+var Input = require('react-bootstrap').Input;
+var ButtonInput = require('react-bootstrap').ButtonInput;
+var Col = require('react-bootstrap').Col;
+var Row = require('react-bootstrap').Row;
+
+var SearchNavBar = require('./search-nav-bar');
 var ListContainer = require('./ListContainer');
 
 var ProductSearchResult = React.createClass({
@@ -11,23 +18,29 @@ var ProductSearchResult = React.createClass({
   },
   mixins: [Navigation, State],
   getInitialState: function () {
-    console.log(this.getPath());
-    console.log(this.getParams());
     return {
       productSearch: this.getParams().productSearch.replace(/'+'/g, ' '),
       location: this.getParams().location.replace(/'+'/g, ' ')
-    }
+    };
   },
   render: function(){
     return (
-      <div className="container">
-        <div className="row">
+      <div className="container-fluid" style={styles.content}>
+        <SearchNavBar />
+        <Row className="">
           <p>This is the product search results page. This is not protected by the route.</p>
           <ListContainer />
-        </div>
+        </Row>
       </div>
-    )
+    );
   }
 });
+
+var styles = {
+  content: {
+    paddingLeft: 0,
+    paddingRight: 0
+  },
+};
 
 module.exports = ProductSearchResult;
